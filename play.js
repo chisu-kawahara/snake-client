c368733e1e76449634610ad53aa47346963d207a
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-15 16:58:43 -0700

    connecting the server

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require("./client");
 
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",  // Use quotes for strings
-    port: 50541            // Port is a number, no quotes needed
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Handle incoming messages from server
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  return conn;
-};
-
 console.log("Connecting ...");
 connect();
+
+// establishes a connection with the game server
+
