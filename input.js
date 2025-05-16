e78cd5e5090884c953e0e9ecf6bd0722aca97f12
Chisu Kawahara <chisu@Chisus-MacBook-Air.local>
2025-05-15 18:03:29 -0700

    input module

--- input.js
+++ input.js
@@ -1,27 +1,32 @@
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true); // Set raw mode to true to receive input as it is typed
-  stdin.setEncoding("utf8"); // Set encoding to utf8 for text input 
-  stdin.resume(); // Start reading from stdin
-  stdin.on("data", handleUserInput); // Listen for data events on stdin
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // code handling the key press will go here
-  if (key === '\u0003') { // Ctrl+C
-    process.exit();
-  if (key === 'w') {
-      connection.write("Move: up");
-
-    }
-  }
-};
-
 let connection;
 
 const setupInput = (conn) => {
   connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);         // Receive input as it is typed
+  stdin.setEncoding("utf8");      // Text encoding
+  stdin.resume();                 // Start reading stdin
+  stdin.on("data", handleUserInput); // Handle user input
+  return stdin;
 };
 
-module.exports = { setupInput }; // Export the setupInput function
+const handleUserInput = function (key) {
+  if (key === '\u0003') { // Ctrl+C
+    process.exit();
+  }
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -4,21 +4,6 @@ const { setupInput } = require("./input");
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true); // Set raw mode to true to receive input as it is typed
-  stdin.setEncoding("utf8"); // Set encoding to utf8 for text input 
-  stdin.resume(); // Start reading from stdin
-  stdin.on("data", handleUserInput); // Listen for data events on stdin
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // code handling the key press will go here
-  if (key === '\u0003') { // Ctrl+C
-    process.exit();
-  }
-};
 console.log("Connecting ...");
 const conn = connect();
 setupInput(conn);
